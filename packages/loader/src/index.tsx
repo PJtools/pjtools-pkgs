8cb65e0be9ba7ad2122822609a7b8b044716120b
PJtools <pjtools@vip.qq.com>
2021-07-16 18:37:51 +0800

    🍻 清空Loader库的测试代码；

--- packages/loader/src/index.tsx
+++ packages/loader/src/index.tsx
@@ -1,10 +1,10 @@
 /**
- * @文件说明: 定义动态异步模块资源文件加载器
+ * @文件说明: 动态异步模块资源文件加载器
  * @创建人: pjtools@vip.qq.com
  * @创建时间: 2021-06-25 12:32:00
  */
 
-import System from 'systemjs';
+// import System from 'systemjs';
 
 class PkgsLoader {
   // 获取当前SystemJS实例对象
@@ -12,25 +12,23 @@ class PkgsLoader {
 
   // 构造函数
   constructor() {
-    this.loader = new System.constructor();
-    this.loader.import('https://unpkg.com/lodash@4.17.10/lodash.js').then((data: any) => {
-      console.log(this.getAll());
-      // console.log(data);
-
-      // console.log(this.loader.get('https://unpkg.com/lodash@4.17.10/lodash.js'));
-    });
-
-    console.log(new Map());
+    // this.loader = new System.constructor();
+    // this.loader.import('https://unpkg.com/lodash@4.17.10/lodash.js').then((data: any) => {
+    //   console.log(this.getAll());
+    //   // console.log(data);
+    //   // console.log(this.loader.get('https://unpkg.com/lodash@4.17.10/lodash.js'));
+    // });
+    // console.log(new Map());
   }
 
   /** 获取已注册的所有模块集合对象 */
   getAll() {
-    const collection = new Map();
-    for (const [id, module] of this.loader.entries()) {
-      collection.set(id, module);
-      console.log('dd', this.loader.get(id));
-    }
-    return collection;
+    // const collection = new Map();
+    // for (const [id, module] of this.loader.entries()) {
+    //   collection.set(id, module);
+    //   console.log('dd', this.loader.get(id));
+    // }
+    // return collection;
   }
 }
 
