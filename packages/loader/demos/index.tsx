af63faa129a4705d5c71cfe8a0de1e3aec3fad1d
PJtools <pjtools@vip.qq.com>
2021-08-26 15:22:10 +0800

    🍻 初步提交动态异步文件加载器代码；

--- packages/loader/demos/index.tsx
+++ packages/loader/demos/index.tsx
@@ -2,7 +2,11 @@ import React from 'react';
 import PkgsLoader from 'pjtools-pkgs-loader';
 
 class Demo extends React.PureComponent {
-  componentDidMount() {}
+  componentDidMount() {
+    const loader = new PkgsLoader();
+
+    console.log(loader.import('https://cdn.bootcdn.net/ajax/libs/lodash.js/4.17.21/lodash.min.js'));
+  }
 
   render() {
     return <div />;
--- packages/loader/src/index.tsx
+++ packages/loader/src/index.tsx
@@ -1,35 +1,12 @@
 /**
- * @文件说明: 动态异步模块资源文件加载器
+ * @文件说明: 动态模块资源文件加载器
  * @创建人: pjtools@vip.qq.com
  * @创建时间: 2021-06-25 12:32:00
  */
 
-// import System from 'systemjs';
-
-class PkgsLoader {
-  // 获取当前SystemJS实例对象
-  loader: any;
-
-  // 构造函数
-  constructor() {
-    // this.loader = new System.constructor();
-    // this.loader.import('https://unpkg.com/lodash@4.17.10/lodash.js').then((data: any) => {
-    //   console.log(this.getAll());
-    //   // console.log(data);
-    //   // console.log(this.loader.get('https://unpkg.com/lodash@4.17.10/lodash.js'));
-    // });
-    // console.log(new Map());
-  }
-
-  /** 获取已注册的所有模块集合对象 */
-  getAll() {
-    // const collection = new Map();
-    // for (const [id, module] of this.loader.entries()) {
-    //   collection.set(id, module);
-    //   console.log('dd', this.loader.get(id));
-    // }
-    // return collection;
-  }
-}
+import PkgsLoader from './loader';
+import type { PkgsLoaderConfig } from './loader';
 
 export default PkgsLoader;
+
+export type { PkgsLoaderConfig };
